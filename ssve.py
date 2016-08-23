dabef63ab57cbf186ba5dc5f992d6b1c59f0b1b6
Joseph Utecht <thenmal@gmail.com>
2016-08-23 12:48:23 -0500

    working version

--- ssve.py
+++ ssve.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import datetime
 
 def parse_source_line(line, sources):
     split = line.find(' ')
@@ -34,6 +35,12 @@ def parse_file(filename):
                     sys.exit(1)
     return parts
 
+def duration(start, end):
+    time_format = '%M:%S.%f'
+    stime = datetime.datetime.strptime(start, time_format)
+    etime = datetime.datetime.strptime(end, time_format)
+    duration = etime - stime
+    return str(duration)
 
 if __name__ == '__main__':
     print('Super Simple Video Editor')
@@ -41,20 +48,25 @@ if __name__ == '__main__':
         print('Incorrect usage, please pass ssve video format file')
     else:
         parts = parse_file(sys.argv[1])
+        outname = os.path.splitext(sys.argv[1])[0]
         print(parts)
+
+        if not os.path.exists('clips'):
+            os.makedirs('clips')
+
         for clip_name in parts['clips']:
             clip = parts['clips'][clip_name]
             source = parts['sources'][clip['source']]
-
-            command = 'ffmpeg -i "{source}" -ss {start} -to {end} -async 1 -acodec copy -vcodec huffyuv clips/{clip}.avi'.format(source=source,
-                                                                                                                        start=clip['start'],
-                                                                                                                        end=clip['end'],
-                                                                                                                        clip=clip_name)
+            dur = duration(clip['start'], clip['end'])
+            command = 'ffmpeg -ss {start} -t {duration} -i "{source}" clips/{clip}.mp4'.format(source=source,
+                                                                                                start=clip['start'],
+                                                                                                duration=dur,
+                                                                                                clip=clip_name)
             print(command)
             os.system(command)
 
-        with open('concat.txt', 'w') as f:
+        with open('clips/concat.txt', 'w') as f:
             for clip in parts['timeline']['order']:
-                f.write("file 'clips/{}.avi'\n".format(clip))
-        command = 'ffmpeg -f concat -i concat.txt -c copy output.mp4'
+                f.write("file '{}.mp4'\n".format(clip))
+        command = 'ffmpeg -f concat -i clips/concat.txt {}.mp4'.format(outname)
         os.system(command)
